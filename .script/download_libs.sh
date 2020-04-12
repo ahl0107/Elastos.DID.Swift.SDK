8676c67c643af679257ab8b26deb37988145e35c
liaihong <478701610@qq.com>
2020-04-12 20:28:22 +0800

    no message

--- .script/download_libs.sh
+++ .script/download_libs.sh
@@ -4,14 +4,11 @@ SCRIPT_PATH="$(cd "$(dirname "$0")" && pwd -P )"
 SCRIPT_DIRNAME="$(basename "${SCRIPT_PATH}")"
 
 LIBPATH=${SCRIPT_PATH}/../Externals/HDKey
-LIBPATHSPV=${SCRIPT_PATH}/../Externals/SPVWrapper/SPVWrapper
 LIBPATTERN="/elastos.*ios_arm64.*gz"
-LIBPATTERNSPV="/elastos.*ios_arm64.*gz"
 LIBDIR="-iphoneos"
 
 if [ $1 = "x64" ] ; then
     LIBPATTERN="/elastos.*ios_x64-hdk.*gz"
-    LIBPATTERNSPV="/elastos.*ios_x64-spv.*gz"
     LIBDIR="-iphonesimulator"
 elif [ $1 = "macOS" ]; then
     LIBPATTERN="/elastos.*darwin_x64.*gz"
@@ -19,15 +16,12 @@ elif [ $1 = "macOS" ]; then
 fi
 
 packageUrl=`curl https://github.com/ahl0107/Elastos.DID.Swift.SDK/releases/tag/internal-test | grep -e $LIBPATTERN -o`
-packageUrlspv=`curl https://github.com/ahl0107/Elastos.DID.Swift.SDK/releases/tag/internal-test | grep -e $LIBPATTERNSPV -o`
 libPackageName=${packageUrl##*/}
-#libPackageNamespv=${packageUrlspv##*/}
 
 cd /tmp
 echo "succeed 1============"
 echo "https://github.com"${packageUrl} >did_libs.txt
 echo "succeed 2============${packageUrl}"
-#echo "https://github.com"${packageUrlspv} >spv_libs.txt
 
 #remove old package
 rm ${libPackageName}
